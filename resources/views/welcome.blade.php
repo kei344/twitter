2f947a656d3f73e854c278d14881dd44e0722bb1
kei.miyoshi <kei344m@gmail.com>
2020-11-15 17:25:56 +0900

    init and toppage

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.share')
+
+@section('content')
+
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Twitter Clone</h1>
+        </div>
+    </div>
+
+@endsection
