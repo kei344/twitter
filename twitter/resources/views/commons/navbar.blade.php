935b36017cb2dfce44f209ca151b08249f09e66b
kei.miyoshi <kei344m@gmail.com>
2020-11-15 17:15:43 +0900

    top page

---
+++ twitter/resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="mb-4 navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-sm container">
+        <a class="navbar-brand" href="https://localhost/twitter/public/">Twitter</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-menu">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Signup</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
